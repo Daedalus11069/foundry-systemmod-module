e90f0228012149157df65d90702ed84d89c51f87
daedalus11069 <daedalus969@gmail.com>
2025-02-04 18:30:43 -0800

    Fix bug from last commit

--- dev-env.js
+++ dev-env.js
@@ -4,6 +4,6 @@ hmr.type = "module";
 document.head.prepend(hmr);
 
 const lib = document.createElement("script");
-lib.src = "/modules/systemmod/src/main.js";
+lib.src = "/modules/systemmod/src/systemmod.js";
 lib.type = "module";
 document.head.appendChild(lib);
